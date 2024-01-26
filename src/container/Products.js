c182bfc06356fde5d762e12e04de457f53be49ff
Rohan <rohansakhare130@gmail.com>
2024-01-26 11:57:32 +0530

    modal add

--- src/container/Products.js
+++ src/container/Products.js
@@ -1,24 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import "../asset/css/container/categorypage.css";
 import "../asset/css/container/product.css";
 import { BiCategory, } from "react-icons/bi";
 import { useNavigate } from 'react-router-dom';
 import ButtonA from '../atoms/ButtonA';
 import SearchBar from '../include/Searchbar'
-import { RiDeleteBin6Line } from "react-icons/ri";
-import { FaRegEdit } from "react-icons/fa";
 import Prodimg from '../asset/images/productimg-1.png';
 import Prodimg1 from '../asset/images/productimg-2.png';
 import Prodimg2 from '../asset/images/productimg-3.png';
 import ProductItem from '../include/ProductItem';
+import Modal from 'react-modal';
+import Out1 from '../asset/images/out-img.png';
+import Screen from '../screen/Screen';
 const Products = () => {
     const navigate = useNavigate();
+    const [isDialogOpen, setIsDialogOpen] = useState(false);
+
     const handleLogout = () => {
         navigate('/addproduct');
     };
-    const handleDeleted = () => {
-        navigate('/deleted');
-    };
+    
+  const handleDeleted = () => {
+    setIsDialogOpen(true);
+  };
+
+  const handleCloseDialog = () => {
+    setIsDialogOpen(false);
+  };
+
+  const handleConfirmDelete = () => {
+    handleCloseDialog();
+    // Perform the actual delete action here if needed
+  };
 
     const products = [
         { id: 123, name: 'Amul Taaza', packSize: '500 ml', category: 'Milk', mrp: 'Rs 27', imgSrc: Prodimg, status: 'Active' },
@@ -81,7 +94,34 @@ const Products = () => {
                 />
             ))}
             </div>
-
+            <Modal
+            isOpen={isDialogOpen}
+            onRequestClose={handleCloseDialog}
+            contentLabel="Confirm Delete"
+            className="modal-main"
+          >
+              <div className="boxFD-cont">
+                <div className="boxFD-heder-main">
+                  <img src={Out1} className="boxFD-img-1" alt={"logo"} />
+                  <p className="boxFD-text1">Delete</p>
+                </div>
+                <p className="boxFD-text3">Are you sure you want to delete?</p>
+    
+                <div className="boxFD-button-main">
+                  <ButtonA
+                    name="Cancel"
+                    className="boxFD-button2"
+                    onClick={handleCloseDialog}
+                  />
+                  <ButtonA
+                    name="Confirm"
+                    className="boxFD-button2 bt2"
+                    onClick={handleConfirmDelete}
+                  />
+                </div>
+              </div>
+           
+          </Modal>
             
         </div>
     )
